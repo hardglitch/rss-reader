f4e721f1eb2d9d9e38718f77f94cfe8462198b3c
hardglitch <lightwave6666@gmail.com>
2023-08-17 17:50:52 +0500

    Area 1 designed

--- src/app.rs
+++ src/app.rs
@@ -40,59 +40,69 @@ impl eframe::App for App {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
+            let main_height = Box::new(ui.available_height());
 
             ui.horizontal(|ui| {
 
-                // Field 1
+                // Area 1
                 ui.vertical(|ui| {
                     ui.set_width(20.0);
-    
-                    let list_text = egui::RichText::new(egui_phosphor::regular::LIST).size(16.0);
-                    let list_btn = egui::Label::new(list_text);
-                    if ui.add(list_btn.sense(egui::Sense::click())).clicked() {
-                        println!("Clicked 1");
-                    };
-    
-                    let star_text = egui::RichText::new(egui_phosphor::regular::STAR).size(16.0);
-                    let star_btn = egui::Label::new(star_text);
-                    if ui.add(star_btn.sense(egui::Sense::click())).clicked() {
-                        println!("Clicked 2");
-                    };
-    
-                    let plus_text = egui::RichText::new(egui_phosphor::regular::PLUS).size(16.0);
-                    let plus_btn = egui::Label::new(plus_text);
-                    if ui.add(plus_btn.sense(egui::Sense::click())).clicked() {
-                        println!("Clicked 3");
-                    };
+                    ui.set_height(*main_height);
 
-                    for ch in chs {
-                
-                        let ch_icon = |ui| {
 
-                            let img = RetainedImage::from_image_bytes(
-                                "favicon.ico",
-                                ch.image.as_slice(),
-                            )
-                            .unwrap();
-    
-                            if img
-                                .show_max_size(ui, Vec2{x:16.0, y:16.0})
-                                .on_hover_text(&ch.title)
-                                .interact(egui::Sense::click())
-                                .clicked() {
-                                    println!("{:?}", &ch.title);
-                                }
+                    let area_1_1 = |ui: &mut egui::Ui| {
+                        let list_text = egui::RichText::new(egui_phosphor::regular::LIST).size(16.0);
+                        let list_btn = egui::Label::new(list_text);
+                        if ui.add(list_btn.sense(egui::Sense::click())).clicked() {
+                            println!("Clicked 1");
                         };
-                        ch_icon(ui);
+        
+                        let star_text = egui::RichText::new(egui_phosphor::regular::STAR).size(16.0);
+                        let star_btn = egui::Label::new(star_text);
+                        if ui.add(star_btn.sense(egui::Sense::click())).clicked() {
+                            println!("Clicked 2");
+                        };
+        
+                        let plus_text = egui::RichText::new(egui_phosphor::regular::PLUS).size(16.0);
+                        let plus_btn = egui::Label::new(plus_text);
+                        if ui.add(plus_btn.sense(egui::Sense::click())).clicked() {
+                            println!("Clicked 3");
+                        };
                     };
+                    ui.with_layout(egui::Layout::top_down(egui::Align::Center), area_1_1);
+
+
+                    let area_1_2 = |ui: &mut egui::Ui| {
+                        for ch in chs {
+                    
+                            let ch_icon = |ui| {
+
+                                let img = RetainedImage::from_image_bytes(
+                                    "favicon.ico",
+                                    ch.image.as_slice(),
+                                )
+                                .unwrap();
+        
+                                if img
+                                    .show_max_size(ui, Vec2{x:16.0, y:16.0})
+                                    .on_hover_text(&ch.title)
+                                    .interact(egui::Sense::click())
+                                    .clicked() {
+                                        println!("{:?}", &ch.title);
+                                    }
+                            };
+                            ch_icon(ui);
+                        }
+                    };
+                    ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), area_1_2);
     
                 });
 
                 
-                // Field 2 and 3
+                // Areas 2 and 3
                 ui.vertical(|ui| {
                     ui.set_height(200.0);
-                    ui.label("Field 2");
+                    ui.label("Area 2");
                 });
 
             });
